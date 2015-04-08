244ce4fb517f31f1c099d90624c5b7c945385a0a
Cory Forsyth <cory.forsyth@gmail.com>
2015-04-07 22:42:58 -0400

    only truefalse and multiple choice questions, add mongo-connect for sessions

---
+++ README.md
@@ -0,0 +1,4 @@
+to do:
+
+ * grading
+ * use session for question-by-question
--- server.js
+++ server.js
@@ -2,11 +2,13 @@ var express = require('express');
 var app = express();
 var port = process.env.PORT || 5000;
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded());
+app.use(bodyParser.urlencoded({extended:true}));
 
 // import mongoose
 var mongoose = require('mongoose');
@@ -18,19 +20,6 @@ var uristring =
   process.env.MONGOHQ_URL ||
   'mongodb://localhost/sample-quizzer';
 
-
-var questionSchema = new mongoose.Schema({
-  name: String,
-  description: String,
-  type: String,
-  answer: String,
-});
-var quizSchema = new mongoose.Schema({
-  name: String,
-  questions: [questionSchema]
-});
-var Quiz = mongoose.model('quizzes', quizSchema);
-
 // Makes connection asynchronously.  Mongoose will queue up database
 // operations and release them when the connection is complete.
 mongoose.connect(uristring, function (err) {
@@ -41,7 +30,35 @@ mongoose.connect(uristring, function (err) {
   }
 });
 
+app.use(session({
+  store: new MongoStore({ mongooseConnection: mongoose.connection }),
+  saveUninitialized: true,
+  secret: 'asdlfkjalsdfkj',
+  resave: true
+}));
+
+var questionSchema = new mongoose.Schema({
+  name: String,
+  description: String,
+  type: String,
+
+  // for multiple-choice:
+  choice1: String,
+  choice2: String,
+  choice3: String,
+  choice4: String,
+
+  answer: String,
+});
+var quizSchema = new mongoose.Schema({
+  name: String,
+  isComplete: Boolean,
+  questions: [questionSchema]
+});
+var Quiz = mongoose.model('quizzes', quizSchema);
+
 app.get('/', function(req, res){
+  console.log('req.session:',req.session);
   Quiz.find({}, function(err, quizzes){
     res.render('index', {quizzes: quizzes});
   });
@@ -54,8 +71,34 @@ app.get('/quizzes/new', function(req, res){
 app.get('/quizzes/:quiz_id', function(req, res){
   var quizId = req.params.quiz_id;
   Quiz.findById(quizId, function(err, quiz) {
-    res.render('quiz', {quiz: quiz});
+    if (quiz.isComplete) {
+      res.redirect('/quizzes/' + quiz.id + '/take');
+    } else {
+      res.render('quiz', {quiz:quiz});
+    }
   });
+});
+
+app.get('/quizzes/:quiz_id/take', function(req, res){
+  var quizId = req.params.quiz_id;
+  Quiz.findById(quizId, function(err, quiz) {
+    res.render('take_quiz', {quiz:quiz});
+  });
+});
+
+// mark the quiz complete
+app.post('/quizzes/:quiz_id/complete', function(req,res){
+  var quizId = req.params.quiz_id;
+  Quiz.findById(quizId, function(err, quiz) {
+    quiz.isComplete = true;
+    quiz.save(function(err){
+      if (err) { console.log('err:' + err); }
+      res.redirect('/quizzes/' + quiz.id);
+    });
+  });
+});
+
+app.post('/quizzes/:quiz_id/answer', function(req,res){
 });
 
 app.get('/quizzes/:quiz_id/questions/new', function(req, res){
@@ -65,6 +108,7 @@ app.get('/quizzes/:quiz_id/questions/new', function(req, res){
   });
 });
 
+// add a new question to a quiz
 app.post('/quizzes/:quiz_id/questions', function(req, res){
   var quizId = req.params.quiz_id;
   Quiz.findById(quizId, function(err, quiz) {
@@ -75,7 +119,6 @@ app.post('/quizzes/:quiz_id/questions', function(req, res){
       answer: req.body.answer
     };
 
-    debugger;
     quiz.questions.push(question);
 
     quiz.save(function(err){
@@ -85,7 +128,7 @@ app.post('/quizzes/:quiz_id/questions', function(req, res){
 });
 
 app.post('/quizzes', function(req,res){
-  var quiz = new Quiz({name: req.body.name});
+  var quiz = new Quiz({isComplete: false, name: req.body.name});
   quiz.save( function(err) {
     res.redirect('/quizzes/' + quiz.id);
   });
